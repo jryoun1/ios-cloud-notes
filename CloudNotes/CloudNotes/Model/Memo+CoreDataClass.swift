42ca6e6443068b827a79c5cfc1213e545da46c08
jae57 <ggapsong@naver.com>
2021-02-26 14:55:35 +0900

    Refactor: core data model 추가, MemoData 제거

---
+++ CloudNotes/CloudNotes/Model/Memo+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Memo+CoreDataClass.swift
+//  CloudNotes
+//
+//  Created by 김지혜 on 2021/02/26.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Memo)
+public class Memo: NSManagedObject {
+
+}
